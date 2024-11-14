53c54faba5386d247d9323148b384b8663df6e56
OscarEscamillaCast <78568136+OscarEscamillaCastillo@users.noreply.github.com>
2024-11-14 16:36:15 -0600

    PRIMER CAMBIO

---
+++ Siegtrack/js/homeAdmin.js
@@ -0,0 +1,4 @@
+// Mensaje de prueba al hacer clic en "Nuevo Usuario"
+document.getElementById("new-user-btn").addEventListener("click", () => {
+    alert("Agregar nuevo usuario aún no implementado.");
+});
